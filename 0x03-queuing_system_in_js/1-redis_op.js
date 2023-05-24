cbae6d38aa9e2a4aaf4238f0c0f6992be29b82cb
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-24 23:02:29 +0100

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,39 @@
+import redis from 'redis';
+
+const client = redis.createClient({
+  url: 'redis://127.0.0.1:6379'
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err);
+});
+
+//Set a value
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error('Error setting value:', err);
+    } else {
+      console.log(`Reply:`, reply);
+    }
+  });
+};
+
+//get the value
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+    console.error('Error getting value:', err);
+    } else {
+    console.log(reply);
+    }
+  });
+};
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+
+  displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  displaySchoolValue('HolbertonSanFrancisco');
+});
