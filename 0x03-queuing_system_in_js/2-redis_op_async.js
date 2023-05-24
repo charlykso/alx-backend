473b03fe8a65ec2135e94a5a54d264811a8da3a1
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-24 23:06:27 +0100

    Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,39 @@
+import redis from 'redis';
+
+const client = redis.createClient({
+  url: 'redis://127.0.0.1:6379'
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err);
+});
+
+//Set a value
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error('Error setting value:', err);
+    } else {
+      console.log(`Reply:`, reply);
+    }
+  });
+};
+
+//get the value
+const displaySchoolValue = async (schoolName) => {
+  await client.get(schoolName, (err, reply) => {
+    if (err) {
+    console.error('Error getting value:', err);
+    } else {
+    console.log(reply);
+    }
+  });
+};
+
+client.on('connect', async () => {
+  console.log('Redis client connected to the server');
+
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+});
