cd5c44f406c5c4ddfd9dbae38909fa44052ec9a4
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-24 22:36:26 +0100

    Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,17 @@
+import redis from 'redis';
+
+const client = redis.createClient({
+  url: 'redis://127.0.0.1:6379'
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: Error', err);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('ready', () => {
+  console.log('Redis client is ready');
+});
